f635c3ed807fb377d8adc6578e563e801cb805ca
swapanpal <swapan.d83@gmail.com>
2020-01-12 01:02:08 +0700

    Created a custom greeting message method

--- method.java
+++ method.java
@@ -1,1 +1,8 @@
 
+/**
+Creates a custom greeting message based on the name.
+@return text greeting
+*/
+private String createCustomGreeting(String firstName, String lastName){
+  return "Welcome, " + firstName + " " + lastName + "!";
+}

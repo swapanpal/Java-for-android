f6c03d989e9625bcf1ccad2570cc7cc45790cbc1
swapanpal <swapan.d83@gmail.com>
2020-01-30 02:33:24 +0700

    if/else and relation statement example added

--- method.java
+++ method.java
@@ -66,3 +66,75 @@ or try below
     private void calculatePrice(int quantity, int pricePerCup) {
         int price = quantity * pricePerCup;
     }
+    
+/**
+if/else statement example
+*/
+// Android for Beginners : If/Else Weather Sample Quiz
+boolean isRaining = true;
+if (isRaining) {
+     Log.v("WeatherActivity", "It's raining, better bring an umbrella.");
+} else {
+     Log.v("WeatherActivity", "It's unlikely to rain.");
+}
+Log.v("WeatherActivity", "Thank you for using the WhetherWeather App.");
+/** output will be 
+It's raining, better bring an umbrella.
+Thank you for using the WhetherWeather App.
+
+*/
+// if statement is false than----
+boolean isRaining = false;
+Log.v("WeatherActivity", "Thank you for using the WhetherWeather App.");
+if (isRaining) {
+    Log.v("WeatherActivity", "It's raining, better bring an umbrella.");
+} else {
+    Log.v("WeatherActivity", "It's unlikely to rain.");
+}
+/**
+output will be
+Thank you for using the WhetherWeather App.
+It's unlikely to rain.
+*/
+
+// If/Else Email Quiz
+int numberOfEmailsInInbox = 0;
+int numberOfDraftEmails = 2;
+String emailMessage = "You have " + numberOfEmailsInInbox + " emails. ";
+String draftMessage = "You have " + numberOfDraftEmails + " email drafts.";
+if (numberOfEmailsInInbox == 0) {
+    emailMessage = "You have no new messages. ";
+}
+
+if (numberOfDraftEmails == 0) {
+    draftMessage = "You have no new drafts.";
+}
+
+Log.v("InboxActivity", emailMessage);
+Log.v("InboxActivity", draftMessage);
+/**
+Output:
+You have no new messages.
+You have 2 email drafts.
+*/
+
+// Android for Beginners : If/Else Smoothie Quiz
+int numberOfSmoothiesTillPrize = 10;
+if (numberOfSmoothiesTillPrize > 9) {
+    Log.v("SmoothieActivity", "Congratulations, you get a free smoothie!");
+    numberOfSmoothiesTillPrize = numberOfSmoothiesTillPrize - 10;
+} else {
+    Log.v("SmoothieActivity", "No free smoothie this time.");
+}
+Log.v("SmoothieActivity", "You currently have " + numberOfSmoothiesTillPrize + " out of 10 smoothies needed for your next free smoothie.");
+
+
+// another example for a game 
+int points = 0;
+if (points == 0){
+textView.setText("losser");
+imageView.setImageResource(R.id.sad);
+}else{
+  textView.setText("Winner");
+imageView.setImageResource(R.id.happy);
+}

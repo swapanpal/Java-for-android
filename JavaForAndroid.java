3557dfe2a25c5730bd836fbe4f8288b888d4398d
swapanpal <swapan.d83@gmail.com>
2020-02-28 13:59:09 +0700

    JavaForAndroid file created

---
+++ JavaForAndroid.java
@@ -0,0 +1,20 @@
+
+
+// Array Example
+// Create an array of words
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+ /*
+ or we can use
+ String words [] = new String [] {"one","two","three","four","five","six","seven","eight","nine","ten"};
+
+  */
